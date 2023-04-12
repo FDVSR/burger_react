8577b9203589db2159bcc98f8d4d447418742769
Violet <veta.xalyutina@mail.ru>
2023-04-12 17:35:10 +0300

    Placing an order on a page + editing the layout

--- src/components/Count/Count.jsx
+++ src/components/Count/Count.jsx
@@ -1,23 +1,22 @@
 import style from "./Count.module.css"
 import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {addProduct, removeProduct} from "../../store/order/orderSlice.js";
 
-export const Count = (props) => {
-    const [count, setCount] = useState(props.count)
 
+
+export const Count = ({count,id}) => {
+    const dispatch = useDispatch();
     const addCount = () => {
-        setCount(count+1);
-    }
-
+        dispatch(addProduct({id}));
+    };
     const removeCount = () => {
-        if (count >-1) {
-            setCount(count-1)
-        }
-
-    }
+        dispatch(removeProduct({id}));
+    };
 
     return(
         <div className={style.count}>
-            <button className={style.minus} onClick={removeCount} disabled={count===0}>-</button>
+            <button className={style.minus} onClick={removeCount}>-</button>
             <p className={style.amount}>{count}</p>
             <button className={style.plus} onClick={addCount}>+</button>
      </div>
